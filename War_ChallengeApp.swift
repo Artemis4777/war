c38e92908f0d325c537232d5ad5ad422a509e995
Artemis47 <cberger0354@gmail.com>
2022-05-09 12:52:36 -0400

    number1

---
+++ War_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_ChallengeApp.swift
+//  War-Challenge
+//
+//  Created by Nosson Berger on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct War_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
